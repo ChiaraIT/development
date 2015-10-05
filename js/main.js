80f8e42f08c82c0cce0554e03182d3feaa30ed3f
ChiaraIT <tecnaviapps6@Tecnaviapps6s-Mac-mini.local>
2015-10-05 17:41:39 +0200

    Progress

--- js/main.js
+++ js/main.js
@@ -8,12 +8,21 @@ mastermind.PlayMGame = function(){
 	var settings = {
 
 		pegColours: ["black","blue","yellow","green","red","orange"],
-		attempsNumber: 10
+		attempsNumber: 10,
+		activeColor: "black",
+		countR:1,
+        add: function(){
+			this.countR ++;
+        }
 		
 	};
 
+
 	var init = function() {
+		printActiveColor();
 		printCololors();
+		printBoard();
+		bindFunctions();
 	};
 
 	//Upload colors
@@ -24,10 +33,79 @@ mastermind.PlayMGame = function(){
 
 		for (var i = 0; i < pegColours.length; i++)
 		{
-			console.log(i);
 			html += '<div class="circle '+ pegColours[i] +'" id="color-'+ pegColours[i] +'"></div>';
 		}
 		mastermindColorsBox.html(html);
+	};
+
+	//bind functions
+	var bindFunctions = function() {
+		$( "div[id|='color']" ).on("click", function(){
+			var color = $(this).attr("id").split("-")[1];
+
+			//Change property
+			settings.activeColor = color;
+			printActiveColor();
+			
+		});
+
+		$( "div[class='empty-circle']" ).on("click", function(){
+			addColor($(this));
+		});
+	};
+
+	//activeColor
+	var printActiveColor = function() {
+		var mastermindColorsBox = $("#mastermind-activeColor");
+		mastermindColorsBox.html(settings.activeColor);
+		
+	};
+
+	//activeColor
+	var printBoard = function() {
+		var tableBoard = $("#mastermind-tableBoard"),
+			html = "",
+			attempsNumber = settings.attempsNumber;
+
+		for (var i = 0; i < attempsNumber; i++)
+		{
+		
+			html += '<tr id="row-' + (i+1) + '">';
+				html += '<td><b>' + (i+1) + '</b></td>';
+                html += '<td align="center"><div class="empty-circle"></div></td>';
+                html += '<td align="center"><div class="empty-circle"></div></td>';
+                html += '<td align="center"><div class="empty-circle"></div></td>';
+                html += '<td align="center"><div class="empty-circle"></div></td>';
+                html += '<td align="center"><!-- --></td>';
+                html += '<td align="center">Check</td>';
+            html += '</tr>';
+		}
+
+
+		tableBoard.html(html);
+		
+	};
+
+	//addColor
+	var addColor = function(subj) {
+
+		var row = subj.parent().parent(),
+		rowNumber = $(row).attr("id").split("-")[1];
+
+		if(rowNumber == settings.countR){
+			subj.css("background-color",settings.activeColor);
+		}else{
+			alert("one row at the time!");
+		}
+		
+	};
+
+	//checkRow
+	var checkRow = function(subj) {
+		
+		//4 color
+		//
+		
 	};
 
 	init.call(this);
