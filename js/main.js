1b543f8bf2d57ba7100edb9b7640e8069e04378b
ChiaraIT <tecnaviapps6@Tecnaviapps6s-Mac-mini.local>
2015-10-05 14:43:54 +0200

    start working on js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 
+mastermind = {};
+
+mastermind.PlayMGame = function(){
+
+	"use strict";
+	
+	var settings = {
+
+		pegColours: ["black","blue","yellow","green","red","orange"],
+		attempsNumber: 10
+		
+	};
+
+	var init = function() {
+		printCololors();
+	};
+
+	//Upload colors
+	var printCololors = function(){
+		var mastermindColorsBox = $("#mastermind-colorsBox"),
+			html = "",
+			pegColours = settings.pegColours;
+
+		for (var i = 0; i < pegColours.length; i++)
+		{
+			console.log(i);
+			html += '<div class="circle '+ pegColours[i] +'" id="color-'+ pegColours[i] +'"></div>';
+		}
+		mastermindColorsBox.html(html);
+	};
+
+	init.call(this);
+};
+
+new mastermind.PlayMGame();
+
